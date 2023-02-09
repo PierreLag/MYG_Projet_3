cc4d61544d85197d2b60d0fb22fe2406d32ce7f5
Pierre Lagoutte <lagoutte_pierre@orange.fr>
2023-02-09 17:19:51 +0100

    Update DiamondMeshGenerator.cs
    Script pour la génération du diamand finalisé. Le modèle 3D est généré comme souhaité, avec 32 triangles et 17 sommets.

--- Assets/Scripts/Interactables/DiamondMeshGenerator.cs
+++ Assets/Scripts/Interactables/DiamondMeshGenerator.cs
@@ -27,6 +27,7 @@ namespace CustomMesh
             mesh.vertices = new Vector3[]
             {
                 Vector3.zero,                           // 0
+
                 new Vector3(0       , 1     , -1.5f),   // 1
                 new Vector3(1       , 1     , -1),      // 2
                 new Vector3(1.5f    , 1     , 0),       // 3
@@ -34,13 +35,25 @@ namespace CustomMesh
                 new Vector3(0       , 1     , 1.5f),    // 5
                 new Vector3(-1      , 1     , 1),       // 6
                 new Vector3(-1.5f   , 1     , 0),       // 7
-                new Vector3(-1      , 1     , -1)       // 8
+                new Vector3(-1      , 1     , -1),      // 8
+
+                new Vector3(0.4f    , 1.2f  , -0.8f),   // 9
+                new Vector3(0.8f    , 1.2f  , -0.4f),   // 10
+                new Vector3(0.8f    , 1.2f  , 0.4f),    // 11
+                new Vector3(0.4f    , 1.2f  , 0.8f),    // 12
+                new Vector3(-0.4f   , 1.2f  , 0.8f),    // 13
+                new Vector3(-0.8f   , 1.2f  , 0.4f),    // 14
+                new Vector3(-0.8f   , 1.2f  , -0.4f),   // 15
+                new Vector3(-0.4f   , 1.2f  , -0.8f),   // 16
+
+                new Vector3(0       , 1.2f  , 0)        // 17
             };
 
             // Direction des vecteurs normaux pour chaque sommets
             mesh.normals = new Vector3[]
             {
                 Vector3.down,
+
                 new Vector3(0, 0, -1),
                 new Vector3(1, 0, -1),
                 new Vector3(1, 0, 0),
@@ -49,6 +62,17 @@ namespace CustomMesh
                 new Vector3(-1, 0, 1),
                 new Vector3(-1, 0, 0),
                 new Vector3(-1, 0, -1),
+
+                new Vector3(1, 0, -2),
+                new Vector3(2, 0, -1),
+                new Vector3(2, 0, 1),
+                new Vector3(1, 0, 2),
+                new Vector3(-1, 0, 2),
+                new Vector3(-2, 0, 1),
+                new Vector3(-2, 0, -1),
+                new Vector3(-1, 0, -2),
+
+                Vector3.up
             };
 
             // Définition de tous les triangles du Mesh. Chaque trio de valeurs consiste à l'indice de chaque sommets de ce triangle, définis auparavant.
@@ -61,7 +85,33 @@ namespace CustomMesh
                 0, 5, 6,
                 0, 6, 7,
                 0, 7, 8,
-                0, 8, 1
+                0, 8, 1,
+
+                1, 9, 2,
+                9, 10, 2,
+                2, 10, 3,
+                10, 11, 3,
+                3, 11, 4,
+                11, 12, 4,
+                4, 12, 5,
+                12, 13, 5,
+                5, 13, 6,
+                13, 14, 6,
+                6, 14, 7,
+                14, 15, 7,
+                7, 15, 8,
+                15, 16, 8,
+                8, 16, 1,
+                16, 9, 1,
+
+                9, 17, 10,
+                10, 17, 11,
+                11, 17, 12,
+                12, 17, 13,
+                13, 17, 14,
+                14, 17, 15,
+                15, 17, 16,
+                16, 17, 9
             };
 
             GetComponent<MeshFilter>().mesh = mesh;
