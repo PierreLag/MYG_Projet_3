6f402d627acd1cf28936f75bd7e3c1cd69c00f53
Pierre Lagoutte <lagoutte_pierre@orange.fr>
2023-02-10 17:18:35 +0100

    Vitesse maximale et rotation joueur
    Une vitesse maximale a été mise en place, changeable dans l'éditeur. Le personnage tourne à présent en fonction de la force appliquée, mais les contrôles sont changés relativement à sa rotation. À résoudre en futur.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,7 +11,11 @@ namespace PlayerScripts
         [SerializeField]
         protected float acceleration = 20f;
         [SerializeField]
+        protected float maxSpeed = 7f;
+        [SerializeField]
         protected float jumpSpeed = 400f;
+        [SerializeField]
+        protected float maxRotationPerso = 1f;
 
         protected PlayerInput inputs;
         private Rigidbody m_rigidbody;
@@ -43,31 +47,36 @@ namespace PlayerScripts
             {
                 if (currentInput[0] == "Forward")
                 {
-                    m_rigidbody.AddForce(Vector3.forward * acceleration, ForceMode.Acceleration);
+                    m_rigidbody.AddRelativeForce(Vector3.forward * acceleration, ForceMode.Acceleration);
                 }
                 if (currentInput[0] == "Backward")
                 {
-                    m_rigidbody.AddForce(Vector3.back * acceleration, ForceMode.Acceleration);
+                    m_rigidbody.AddRelativeForce(Vector3.back * acceleration, ForceMode.Acceleration);
                 }
             }
             if (currentInput[1] != "")
             {
                 if (currentInput[1] == "Left")
                 {
-                    m_rigidbody.AddForce(Vector3.left * acceleration, ForceMode.Acceleration);
+                    m_rigidbody.AddRelativeForce(Vector3.left * acceleration, ForceMode.Acceleration);
                 }
                 if (currentInput[1] == "Right")
                 {
-                    m_rigidbody.AddForce(Vector3.right * acceleration, ForceMode.Acceleration);
+                    m_rigidbody.AddRelativeForce(Vector3.right * acceleration, ForceMode.Acceleration);
                 }
             }
             if (currentInput[2] != "")
             {
                 if (currentInput[2] == "Jump" && Mathf.MoveTowards(m_rigidbody.velocity.y, 0, 0.001f) == 0)
                 {
-                    m_rigidbody.AddForce(Vector3.up * jumpSpeed, ForceMode.Acceleration);
+                    m_rigidbody.AddRelativeForce(Vector3.up * jumpSpeed, ForceMode.Acceleration);
                 }
             }
+            if (m_rigidbody.velocity.magnitude > maxSpeed)
+                m_rigidbody.velocity = m_rigidbody.velocity.normalized * maxSpeed;
+
+            if (m_rigidbody.velocity.magnitude > 0.01)
+                m_rigidbody.rotation = Quaternion.RotateTowards(m_rigidbody.rotation, Quaternion.LookRotation(new Vector3(m_rigidbody.velocity.x, 0, m_rigidbody.velocity.z)), maxRotationPerso);
 
             m_animator.SetFloat("velocity_player", m_rigidbody.velocity.magnitude);
         }
