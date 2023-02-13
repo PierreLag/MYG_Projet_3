5e843e2ef86dda984c290f45e4c1be331345f80e
Pierre Lagoutte <lagoutte_pierre@orange.fr>
2023-02-13 15:20:54 +0100

    Modifications input, triple caméras, et barrières
    Changement du traitement des inputs en dictionnaire, afin de faciliter le changement de touches en cours de partie. Ajout de cinemachines supplémentaires afin de pouvoir changer de perspectives de vues entre normale, en hauteur, et vu du dessus. Ajout d'un prefab de barrières en bois.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 namespace PlayerScripts
 {
@@ -18,8 +19,13 @@ namespace PlayerScripts
         protected float maxRotationSpeed = 1f;
 
         [SerializeField]
-        private Transform mainCameraTransform;
+        private CinemachineVirtualCamera mainCamera;
+        [SerializeField]
+        private CinemachineVirtualCamera topDownCamera;
+        [SerializeField]
+        private CinemachineVirtualCamera highCamera;
 
+        private CinemachineVirtualCamera currentCamera;
         protected PlayerInput inputs;
         private Rigidbody m_rigidbody;
         private Animator m_animator;
@@ -29,6 +35,9 @@ namespace PlayerScripts
             inputs = GetComponent<PlayerInput>();
             m_rigidbody = GetComponent<Rigidbody>();
             m_animator = GetComponent<Animator>();
+
+            mainCamera.Priority = 10;
+            currentCamera = mainCamera;
         }
 
         // Start is called before the first frame update
@@ -50,22 +59,22 @@ namespace PlayerScripts
             {
                 if (currentInput[0] == "Forward")
                 {
-                    m_rigidbody.AddForce(mainCameraTransform.forward * acceleration, ForceMode.Acceleration);
+                    m_rigidbody.AddForce(new Vector3(currentCamera.transform.forward.x, 0, currentCamera.transform.forward.z).normalized * acceleration, ForceMode.Acceleration);
                 }
                 if (currentInput[0] == "Backward")
                 {
-                    m_rigidbody.AddForce(mainCameraTransform.forward * -acceleration, ForceMode.Acceleration);
+                    m_rigidbody.AddForce(new Vector3(currentCamera.transform.forward.x, 0, currentCamera.transform.forward.z).normalized * -acceleration, ForceMode.Acceleration);
                 }
             }
             if (currentInput[1] != "")
             {
                 if (currentInput[1] == "Left")
                 {
-                    m_rigidbody.AddForce(mainCameraTransform.right * -acceleration, ForceMode.Acceleration);
+                    m_rigidbody.AddForce(currentCamera.transform.right * -acceleration, ForceMode.Acceleration);
                 }
                 if (currentInput[1] == "Right")
                 {
-                    m_rigidbody.AddForce(mainCameraTransform.right * acceleration, ForceMode.Acceleration);
+                    m_rigidbody.AddForce(currentCamera.transform.right * acceleration, ForceMode.Acceleration);
                 }
             }
             if (currentInput[2] != "")
@@ -75,6 +84,27 @@ namespace PlayerScripts
                     m_rigidbody.AddForce(Vector3.up * jumpSpeed, ForceMode.Acceleration);
                 }
             }
+
+            if (currentInput[3] != "")
+            {
+                if (currentInput[3] == "Look Up")
+                {
+                    highCamera.Priority = 11;
+                    currentCamera = highCamera;
+                }
+                if (currentInput[3] == "Look Down")
+                {
+                    topDownCamera.Priority = 11;
+                    currentCamera = topDownCamera;
+                }
+            } else
+            {
+                topDownCamera.Priority = 0;
+                highCamera.Priority = 0;
+                mainCamera.MoveToTopOfPrioritySubqueue();
+                currentCamera = mainCamera;
+            }
+
             if (m_rigidbody.velocity.magnitude > maxSpeed)
                 m_rigidbody.velocity = m_rigidbody.velocity.normalized * maxSpeed;
 
--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -16,16 +16,35 @@ namespace PlayerScripts
         protected KeyCode keyRight = KeyCode.D;
         [SerializeField]
         protected KeyCode keyJump = KeyCode.Space;
+        [SerializeField]
+        protected KeyCode keyLookUp = KeyCode.R;
+        [SerializeField]
+        protected KeyCode keyLookDown = KeyCode.F;
+
+        protected Dictionary<string, KeyCode> keyMap;
+
+        PlayerInput()
+        {
+            keyMap = new Dictionary<string, KeyCode>();
+            keyMap.Add("Forward", keyForward);
+            keyMap.Add("Backward", keyBackward);
+            keyMap.Add("Left", keyLeft);
+            keyMap.Add("Right", keyRight);
+            keyMap.Add("Jump", keyJump);
+
+            keyMap.Add("Look Up", keyLookUp);
+            keyMap.Add("Look Down", keyLookDown);
+        }
 
         public string[] GetInput()
         {
-            string[] input = { "", "", "" };
+            string[] input = { "", "", "", "" };
 
-            if (Input.GetKey(keyForward))
+            if (Input.GetKey(keyMap["Forward"]))
             {
                 input[0] = "Forward";
             }
-            if (Input.GetKey(keyBackward))
+            if (Input.GetKey(keyMap["Backward"]))
             {
                 if (input[0] == "Forward")
                     input[0] = "";
@@ -33,11 +52,11 @@ namespace PlayerScripts
                     input[0] = "Backward";
             }
 
-            if (Input.GetKey(keyLeft))
+            if (Input.GetKey(keyMap["Left"]))
             {
                 input[1] = "Left";
             }
-            if (Input.GetKey(keyRight))
+            if (Input.GetKey(keyMap["Right"]))
             {
                 if (input[1] == "Left")
                     input[1] = "";
@@ -45,9 +64,21 @@ namespace PlayerScripts
                     input[1] = "Right";
             }
 
-            if (Input.GetKey(keyJump))
+            if (Input.GetKey(keyMap["Jump"]))
             {
                 input[2] = "Jump";
+            }
+
+            if (Input.GetKey(keyMap["Look Up"]))
+            {
+                input[3] = "Look Up";
+            }
+            if (Input.GetKey(keyMap["Look Down"]))
+            {
+                if (input[3] == "Look Up")
+                    input[3] = "";
+                else
+                    input[3] = "Look Down";
             }
 
             return input;
