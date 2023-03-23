f9ff1e03398fd26beaa2a67ee1d749d7fb302a68
Pierre Lagoutte <lagoutte_pierre@orange.fr>
2023-03-23 17:32:31 +0100

    Finalisation structure WindJumping
    Finalisation de la structure WindJumping, avec des ventilateurs qui bougent et des pièces haut dans le ciel, ayant plus de chances d'être de haute valeur.

--- Assets/Scripts/Events/MovingObjectController.cs
+++ Assets/Scripts/Events/MovingObjectController.cs
@@ -2,74 +2,138 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingObjectController : MonoBehaviour
+namespace CustomEvents
 {
-    [SerializeField]
-    protected Vector3[] pointsPassage;
-    [SerializeField]
-    [Range(0f, 1f)]
-    protected float offsetPosition = 0f;
-    [SerializeField]
-    protected bool isLooping = true;
-
-    protected float totalDistance;
-    protected float[] distanceList;
-
-    // Start is called before the first frame update
-    void Awake()
+    [SelectionBase]
+    public class MovingObjectController : MonoBehaviour
     {
-        totalDistance = 0;
-        if (isLooping)
-        {
-            distanceList = new float[pointsPassage.Length];
-            for (int i = 1; i < pointsPassage.Length; i++)
-            {
-                distanceList[i - 1] = (pointsPassage[i] - pointsPassage[i - 1]).magnitude;
-                totalDistance += distanceList[i - 1];
-            }
-            distanceList[distanceList.Length - 1] = (pointsPassage[0] - pointsPassage[pointsPassage.Length - 1]).magnitude;
-            totalDistance += distanceList[distanceList.Length - 1];
-        }
-        else
-        {
-            distanceList = new float[pointsPassage.Length - 1];
-            for (int i = 1; i < pointsPassage.Length; i++)
-            {
-                distanceList[i - 1] = (pointsPassage[i] - pointsPassage[i - 1]).magnitude;
-                totalDistance += distanceList[i - 1];
-            }
-            totalDistance *= 2;
-        }
+        [SerializeField]
+        protected Vector3[] checkpointNodes;
+        [SerializeField]
+        [Range(0f, 1f)]
+        protected float offsetPosition = 0f;
+        [SerializeField]
+        protected bool isLooping = true;
+        [SerializeField]
+        protected float speed = 2f;
 
-        float remainingOffset = totalDistance * offsetPosition;
-        if (isLooping)
-        {
-            int i;
-            for (i = 0; remainingOffset >= distanceList[i]; i++)
-            {
+        protected float totalDistance;
+        protected float[] distanceList;
+        protected bool isGoingBack;
+        protected int latestCheckpoint;
 
-            }
-        }
-        else
+        void Awake()
         {
-            int i;
-            for (i = 0; i < distanceList.Length && remainingOffset >= distanceList[i]; i++)
+            totalDistance = 0;
+            if (isLooping)
             {
-
-            }
-            if (remainingOffset >= distanceList[distanceList.Length - 1])
-            {
-                for (i = distanceList.Length - 1; i > 0 && remainingOffset >= distanceList[i]; i--)
+                distanceList = new float[checkpointNodes.Length];
+                for (int i = 1; i < checkpointNodes.Length; i++)
                 {
+                    distanceList[i - 1] = (checkpointNodes[i] - checkpointNodes[i - 1]).magnitude;
+                    totalDistance += distanceList[i - 1];
+                }
+                distanceList[distanceList.Length - 1] = (checkpointNodes[0] - checkpointNodes[checkpointNodes.Length - 1]).magnitude;
+                totalDistance += distanceList[distanceList.Length - 1];
+            }
+            else
+            {
+                distanceList = new float[checkpointNodes.Length - 1];
+                for (int i = 1; i < checkpointNodes.Length; i++)
+                {
+                    distanceList[i - 1] = (checkpointNodes[i] - checkpointNodes[i - 1]).magnitude;
+                    totalDistance += distanceList[i - 1];
+                }
+                totalDistance *= 2;
+            }
 
+            float remainingOffset = totalDistance * offsetPosition;
+            if (isLooping)
+            {
+                int i;
+                for (i = 0; remainingOffset >= distanceList[i]; i++)
+                {
+                    remainingOffset -= distanceList[i];
+                }
+
+                if (i == checkpointNodes.Length - 1)
+                    transform.localPosition = Vector3.MoveTowards(checkpointNodes[i], checkpointNodes[0], remainingOffset);
+                else
+                    transform.localPosition = Vector3.MoveTowards(checkpointNodes[i], checkpointNodes[i + 1], remainingOffset);
+                latestCheckpoint = i;
+            }
+            else
+            {
+                int i;
+                isGoingBack = false;
+                for (i = 0; i < distanceList.Length && remainingOffset >= distanceList[i]; i++)
+                {
+                    remainingOffset -= distanceList[i];
+                }
+                if (i == distanceList.Length)
+                {
+                    isGoingBack = true;
+                    for (i = distanceList.Length - 1; remainingOffset >= distanceList[i]; i--)
+                    {
+                        remainingOffset -= distanceList[i];
+                    }
+                }
+
+                if (isGoingBack)
+                {
+                    transform.localPosition = Vector3.MoveTowards(checkpointNodes[i + 1], checkpointNodes[i], remainingOffset);
+                    latestCheckpoint = i + 1;
+                }
+                else
+                {
+                    transform.localPosition = Vector3.MoveTowards(checkpointNodes[i], checkpointNodes[i + 1], remainingOffset);
+                    latestCheckpoint = i;
                 }
             }
         }
-    }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        
+        void FixedUpdate()
+        {
+            if (isLooping)
+            {
+                if (latestCheckpoint == checkpointNodes.Length - 1)
+                {
+                    transform.localPosition = Vector3.MoveTowards(transform.localPosition, checkpointNodes[0], Time.fixedDeltaTime * speed);
+                    if (transform.localPosition == checkpointNodes[0])
+                        latestCheckpoint = 0;
+                }
+                else
+                {
+                    transform.localPosition = Vector3.MoveTowards(transform.localPosition, checkpointNodes[latestCheckpoint + 1], Time.fixedDeltaTime * speed);
+                    if (transform.localPosition == checkpointNodes[latestCheckpoint + 1])
+                        latestCheckpoint++;
+                }
+            }
+            else
+            {
+                if (isGoingBack)
+                {
+                    transform.localPosition = Vector3.MoveTowards(transform.localPosition, checkpointNodes[latestCheckpoint - 1], Time.fixedDeltaTime * speed);
+                    if (transform.localPosition == checkpointNodes[latestCheckpoint - 1])
+                    {
+                        latestCheckpoint--;
+
+                        if (latestCheckpoint == 0)
+                            isGoingBack = false;
+                    }
+                }
+                else
+                {
+                    transform.localPosition = Vector3.MoveTowards(transform.localPosition, checkpointNodes[latestCheckpoint + 1], Time.fixedDeltaTime * speed);
+                    if (transform.localPosition == checkpointNodes[latestCheckpoint + 1])
+                    {
+                        latestCheckpoint++;
+
+                        if (latestCheckpoint == checkpointNodes.Length - 1)
+                            isGoingBack = true;
+                    }
+                }
+            }
+        }
     }
 }
